945219edfc6ebb5aea6fc93744147cf95ef35fdc
roguesyko <roguesyko@gmail.com>
2016-12-06 15:59:26 -0500

    Put the scythe to it

--- reaper.mk
+++ reaper.mk
@@ -1,19 +1,15 @@
 $(call inherit-product, device/lge/d852/full_d852.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+# Inherit some common Reaper stuff.
+$(call inherit-product, vendor/reaper/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/reaper/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := broken_d852
+PRODUCT_NAME := reaper_d852
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_bell_ca" \
     BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1421650137/1421650137:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1421650137 1421650137 release-keys"
-
-# Broken Device Maintainers
-    PRODUCT_BUILD_PROP_OVERRIDES += \
-  	DEVICE_MAINTAINERS="Joe (roguesyko) Repasy"
